ddf71ad3084571430ac8d77416f1e6dc1e43dfaf
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-07 23:59:55 +0900

    click

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./styles.css";
+// import { Page } from "./";
+// import { registerADAccountInformation } from "./handle";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* <Page /> */}
+      親コンポーネント
+      {/* <button type="button" onClick={registerADAccountInformation}>
+        click!!
+      </button> */}
     </div>
   );
 }
